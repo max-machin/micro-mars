38151ee58971c85ab227aebcc65bdb12992f3848
Tchessy <tchessy@agence-map.com>
2024-05-25 19:27:32 +0200

    Added auth service

--- service-auth/src/main.ts
+++ service-auth/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
