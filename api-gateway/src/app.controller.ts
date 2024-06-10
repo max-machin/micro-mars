cf8d679e58b6a3467e1b9ebad0f1635fa0ba23ca
Machin Max <machin.max@laplateforme.io>
2024-06-10 16:03:46 +0200

    ajout de la route 'order'

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -3,7 +3,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateOrderRequest } from './create-order-request.dto';
 
-@Controller()
+@Controller('order')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
