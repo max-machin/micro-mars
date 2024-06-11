f0d8591f6df0683abd291ed7709238881f0cc9b2
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-06-11 21:25:43 +0200

    FEAT - creat simple root for product

---
+++ api-gateway/src/entities/product/product.controller.ts
@@ -0,0 +1,32 @@
+//Création du controller liée à l'entité product.enity.ts
+
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.entity';
+
+@Controller('products')
+export class ProductController {
+    constructor(private productService: ProductService) {}
+
+    @Get()
+    index(): Promise<Product[]> {
+        return this.productService.findAll();
+    }
+
+    @Post('create')
+    async create(@Body() productData: Product): Promise<any> {
+        return this.productService.create(productData);
+    }
+
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() productData: Product): Promise<any> {
+        productData.id = Number(id);
+        console.log('Update #' + productData.id)
+        return this.productService.update(productData);
+    }
+
+    @Delete(':id/delete')
+    async delete(@Param('id') id): Promise<any> {
+        return this.productService.delete(id);
+    }
+}
