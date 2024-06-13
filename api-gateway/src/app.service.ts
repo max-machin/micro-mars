81b1c0e80a27e724bc2cc106d243e610cddf2f72
Tchessy <tchessy@agence-map.com>
2024-06-13 13:37:45 +0200

    authentication error message ok

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -36,6 +36,14 @@ export class AppService {
     password: string,
     email: string,
   ): Promise<any> {
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = this.userRepository.create({
       firstname,
@@ -43,23 +51,44 @@ export class AppService {
       password: hashedPassword,
       email,
     });
-    await this.userRepository.save(user);
 
-    this.kafkaClient.emit('user_registered', {
-      email,
-    });
+    try {
+      await this.userRepository.save(user);
 
-    return user;
+      this.kafkaClient.emit('user_registered', {
+        email,
+      });
+
+      return { message: 'User registered successfully', user };
+    } catch (error) {
+      throw new HttpException(
+        'Error registering user: ' + error.message,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async login(email: string, password: string): Promise<any> {
     const user = await this.userRepository.findOne({ where: { email } });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload = { sub: user.id };
-      return {
-        access_token: this.jwtService.sign(payload),
-      };
+
+    if (!user) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
-    throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const payload = { sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
